4cb52731db6c7a4f1c03c755e52d1fe81606cb15
goncy <gonzalo.pozzo4@gmail.com>
2021-02-11 11:50:40 -0300

    Update deps

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,8 +1,11 @@
 import * as express from "express";
+import * as cors from "cors";
 
 import {PORT} from "./constants";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", async (_req: express.Request, res: express.Response) => res.send("Hello Coderio!"));
 
